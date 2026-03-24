212795770498058dee1713374119ec82eeb06257
Iwestlauer <121632983+Iwestlauer@users.noreply.github.com>
2026-03-24 05:28:18 +0000

    Deploying to gh-pages from @ Iwestlauer/iwestlauer.github.io@856a139c4168b3f28a888f36d37384d51739802d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,9 +461,9 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-my-first-r-shiny-page",
+          title: 'My First R Shiny Page',
+          description: "an exploratory data analysis of attrition data",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
